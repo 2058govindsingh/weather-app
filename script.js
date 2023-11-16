7f7f6a70e300d328a6184c62a4b0bebfcb97c8d0
Govind Singh <govindyadav2058@gmail.com>
2023-11-16 05:36:31 +0530

    Data cached in Local storage

--- script.js
+++ script.js
@@ -6,10 +6,20 @@ const cityTime = document.getElementById('city-time');
 const cityTemp = document.getElementById('city-temp');
 
 async function getData(city) {
+  city = String(city)
+  city = city.toLowerCase();
+  const serializedItem = await localStorage.getItem(city);
+  if(serializedItem != null)
+  {
+    console.log("Found in Local Storage");
+    return await JSON.parse(serializedItem);
+  }
   const promise = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=afe6067841744e30bf9123743231311&q=${city}&aqi=yes`
   );
-  return await promise.json();
+  const result = await promise.json();
+  localStorage.setItem(city, JSON.stringify(result))
+  return result;
 }
 
 button.addEventListener('click', async () => {
@@ -21,6 +31,3 @@ button.addEventListener('click', async () => {
   cityTemp.innerText = data.current.temp_c;
 })
 
-window.addEventListener("load", () => {
-    const value = 
-})
